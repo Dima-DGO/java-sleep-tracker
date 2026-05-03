43e5a98e77daa2c6560e62338d2e1d8d76147182
Dima-DGO <oleshkodg@yandex.ru>
2026-05-04 00:40:43 +0700

    Небольшие правки

--- src/main/java/ru/yandex/practicum/sleeptracker/MinDurationFunction.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/MinDurationFunction.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.sleeptracker;
 
 import java.util.List;
-import java.util.OptionalLong;
 
 public class MinDurationFunction implements SleepAnalysisFunction {
     @Override
